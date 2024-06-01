e247749a5c8165543b58065e2873dbb597714c63
Tudor Crisan <crisantudor17@gmail.com>
2024-06-01 14:49:18 +0300

    initial commit

---
+++ src/Engine.java
@@ -0,0 +1,15 @@
+public class Engine {
+    private String type;
+
+    public Engine(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Engine engine = new Engine("V8");
+        Car car = new Car("Mustang", engine);
+    }
+}
